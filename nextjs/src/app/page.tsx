4dbeb343b5f8a101e6d31340f662ff6644394f2c
Onlook <erik@onlook.dev>
2024-01-29 21:03:04 +0000

    Adding onlook comments to multiple files

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -3,13 +3,16 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
+// onlook edits
     <main className={styles.main}>
       <div className={styles.description}>
+// onlook edits
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
+// onlook edits
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
@@ -28,6 +31,7 @@ export default function Home() {
         </div>
       </div>
 
+// onlook edits
       <div className={styles.center}>
         <Image
           className={styles.logo}
