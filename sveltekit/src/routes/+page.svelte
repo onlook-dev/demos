13c4e7edf5cceef1a700e1818c1e3d6a6ec53ad7
Onlook <erik@onlook.dev>
2024-02-28 21:59:49 +0000

    Adding onlook comments to multiple files

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -10,7 +10,7 @@
 </svelte:head>
 
 <section>
-	<h1 class="bg-black font-bold text-red-500">
+	<h1 class="text-red-500 bg-black font-bold">
 		<span class="welcome">
 			<picture>
 				<source srcset={welcome} type="image/webp" />
